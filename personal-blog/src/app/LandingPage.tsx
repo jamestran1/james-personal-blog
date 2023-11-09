0a71e5a68ca1561c795dba22f991f620e4e7c90b
Quang Tran <quang.tran@episerver.com>
2023-11-09 11:24:01 +0700

    Modify style, add image and footer (#3)
    Co-authored-by: Quang Tran <jametran0305@outlook.com>

--- personal-blog/src/app/LandingPage.tsx
+++ personal-blog/src/app/LandingPage.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { LandingPage } from "../../generated/graphql"
 import parse from 'html-react-parser'
 
@@ -8,9 +9,15 @@ type LandingPageProps = {
 function LandingPage({content}: LandingPageProps) {
     return (
         <div>
-            <h1 data-edit-edit="Title">{content.Title}</h1>
-            <h2 data-edit-edit="SubTitle">{content.SubTitle}</h2>
+            <div className="grid justify-items-center">
+                <h1 className="text-3xl" data-edit-edit="Title">{content.Title}</h1>
+                <h2 className="text-1xl underline decoration-wavy" data-edit-edit="SubTitle">{content.SubTitle}</h2>
+            </div>
+            <Image src={content.MainImage?.Url || ''} width={2688} height={1536} alt='zen coder'/>
             <span data-edit-edit="MainBody">{parse(content.MainBody || '')}</span>
+            <div>
+                <span className='text-right'>{content.FooterText}</span>
+            </div>
         </div>
     )
 }
