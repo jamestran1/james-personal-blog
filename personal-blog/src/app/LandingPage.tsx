8cb56c2cc1cb262d3e9c31f2593b73a1814a5814
Quang Tran <jametran0305@outlook.com>
2023-11-10 00:49:35 +0700

    Modify data-epi-edit attribute

--- personal-blog/src/app/LandingPage.tsx
+++ personal-blog/src/app/LandingPage.tsx
@@ -10,13 +10,13 @@ function LandingPage({content}: LandingPageProps) {
     return (
         <div>
             <div className="grid justify-items-center">
-                <h1 className="text-3xl" data-edit-edit="Title">{content.Title}</h1>
-                <h2 className="text-1xl underline decoration-wavy" data-edit-edit="SubTitle">{content.SubTitle}</h2>
+                <h1 className="text-3xl" data-epi-edit="Title">{content.Title}</h1>
+                <h2 className="text-1xl underline decoration-wavy" data-epi-edit="SubTitle">{content.SubTitle}</h2>
             </div>
-            <Image src={content.MainImage || ''} width={2688} height={1536} alt='zen coder'/>
-            <span data-edit-edit="MainBody">{parse(content.MainBody || '')}</span>
+            <Image data-epi-edit="MainImage" src={content.MainImage || ''} width={2688} height={1536} alt='zen coder'/>
+            <span data-epi-edit="MainBody">{parse(content.MainBody || '')}</span>
             <div>
-                <span className='text-right'>{content.FooterText}</span>
+                <span data-epi-edit="FooterText" className='text-right'>{content.FooterText}</span>
             </div>
         </div>
     )
